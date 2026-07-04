ece7fc9762b5896ba4e259550ba923fc357c61cf
rahmonovisroiljon78-beep <rahmonovisroiljon78@gmail.com>
2026-07-04 14:59:24 +0500

    Update main.py with Purchases and Sales API routers

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 from app.database import engine, Base
 from app.api.products import router as product_router
+from app.api.purchases import router as purchase_router
+from app.api.sales import router as sales_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Dokonchi_bola")
 
 app.include_router(product_router)
+app.include_router(purchase_router)
+app.include_router(sales_router)
 
 
 @app.get("/")
